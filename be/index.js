a6f9097eda080640caca493d47bfb8b5aa2fdf25
pippuu <rafiffaustaks@gmail.com>
2023-08-17 11:53:09 +0700

    fix

--- be/index.js
+++ be/index.js
@@ -1,20 +1,20 @@
 // Import modules
 import express from "express";
-import { config } from "./config/config.js";
+import { config } from "./src/config/config.js";
 import mongoose from "mongoose";
-import { InitDB } from "./infrastructure/db.js";
-import ProductService from "./services/product_services.js";
-import ProductRepository from "./repository/product_repo.js";
-import ProductController from "./controllers/product_controller.js";
-import ProductRouter from "./routes/product_routes.js";
-import VideoRepository from "./repository/video_repo.js";
-import VideoService from "./services/video_services.js";
-import VideoController from "./controllers/video_controller.js";
-import VideoRouter from "./routes/video_routes.js";
-import CommentRepository from "./repository/comment_repo.js";
-import CommentService from "./services/comment_services.js";
-import CommentController from "./controllers/comment_controller.js";
-import CommentRouter from "./routes/comment_routes.js";
+import { InitDB } from "./src/infrastructure/db.js";
+import ProductService from "./src/services/product_services.js";
+import ProductRepository from "./src/repository/product_repo.js";
+import ProductController from "./src/controllers/product_controller.js";
+import ProductRouter from "./src/routes/product_routes.js";
+import VideoRepository from "./src/repository/video_repo.js";
+import VideoService from "./src/services/video_services.js";
+import VideoController from "./src/controllers/video_controller.js";
+import VideoRouter from "./src/routes/video_routes.js";
+import CommentRepository from "./src/repository/comment_repo.js";
+import CommentService from "./src/services/comment_services.js";
+import CommentController from "./src/controllers/comment_controller.js";
+import CommentRouter from "./src/routes/comment_routes.js";
 import cors from "cors";
 import http from "http";
 
@@ -34,14 +34,14 @@ function main() {
   const videoService = new VideoService(videoRepo);
   const videoController = new VideoController(videoService);
   const videoRoutes = new VideoRouter(videoController);
-  videoRoutes.setupRoutes();  
+  videoRoutes.setupRoutes();
 
   // Create comment layers (repo, service, controller)
   const commentRepo = new CommentRepository();
   const commentService = new CommentService(commentRepo);
   const commentController = new CommentController(commentService);
   const commentRoutes = new CommentRouter(commentController);
-  commentRoutes.setupRoutes();  
+  commentRoutes.setupRoutes();
 
   // Create express app
   const app = express();
@@ -49,19 +49,21 @@ function main() {
   app.use(cors());
 
   app.get("/", (req, res) => {
-    res.status(201).json({message: "Connected to Backend!"});
-});
+    res.status(201).json({ message: "Connected to Backend!" });
+  });
 
   app.use("/api/product", productRoutes.getRoutes());
   app.use("/api/video", videoRoutes.getRoutes());
   app.use("/api/comment", commentRoutes.getRoutes());
 
-  const server = http.createServer(app)
+  const server = http.createServer(app);
 
   // Run server
   server.listen(config.app.port, () => {
     console.log(`App listening on port ${config.app.port}`);
   });
+
+  module.exports = server;
 }
 
 // Run the whole code
